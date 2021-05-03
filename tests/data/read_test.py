42c1f81d048bfc0450b058058295ace8984b0aaa
Kirill Brodt <cyrill.brodt@gmail.com>
2021-05-02 22:56:29 -0400

    Update workflows and test

--- tests/data/read_test.py
+++ tests/data/read_test.py
@@ -1,9 +1,13 @@
-from ml_project.data import read_data
+import tempfile
 
-DATA_PATH = "data/raw/heart.csv"
+import pandas as pd
+
+from ml_project.data import read_data
 
 
 def test_read_data():
-    data = read_data(DATA_PATH)
+    f, path = tempfile.mkstemp()
+    pd.DataFrame([[1, 2], ["a", "b"]]).to_csv(path, index=False)
+    data = read_data(path)
 
-    assert data.shape == (303, 14)
+    assert data.shape == (2, 2)
