0d62a5ec5c288294859aab052478e06b08b2e7cd
Kirill Brodt <cyrill.brodt@gmail.com>
2021-05-03 20:43:01 -0400

    Add predict script

---
+++ predict.py
@@ -0,0 +1,46 @@
+import logging
+import logging.config
+from pathlib import Path
+
+import hydra
+import pandas as pd
+import yaml
+from hydra.utils import to_absolute_path
+
+from ml_project.apis import load_model, predict_model
+from ml_project.config import build_config
+from ml_project.data import read_data
+from ml_project.features import make_features
+
+
+def setup_logging(logging_config_filepath):
+    with open(logging_config_filepath) as config_in:
+        config = yaml.safe_load(config_in)
+        logging.config.dictConfig(config)
+
+
+def predict(cfg):
+    config = build_config(cfg)
+
+    df = read_data(to_absolute_path(config.test_data_path))
+
+    path_to_load = Path(to_absolute_path(config.experiment_path))
+    model, transformer = load_model(path_to_load / config.output_model_fname)
+    features = make_features(transformer, df)
+
+    predicts = predict_model(model, features, return_proba=True)
+    predicts = predicts[:, 1]
+
+    pd.DataFrame(predicts, columns=["target"]).to_csv(
+        to_absolute_path(config.predict_path), index=False
+    )
+
+
+@hydra.main()
+def main(cfg):
+    setup_logging(to_absolute_path(cfg.logging_path))
+    predict(cfg)
+
+
+if __name__ == "__main__":
+    main()
