22b26f28824900c6ff3f8556dcb1d88101b56ae4
Kirill Brodt <cyrill.brodt@gmail.com>
2021-05-05 17:44:01 -0400

    Fix path in workflows

--- ml_project/setup.py
+++ ml_project/setup.py
@@ -2,8 +2,20 @@ from setuptools import find_packages, setup
 
 
 def parse_requirements(path):
+    def _parse_requirements(path):
+        with open(path) as fin:
+            return [line.strip() for line in fin]
+
     with open(path) as fin:
-        return [line.strip() for line in fin]
+        reqs = []
+        for line in fin:
+            if line.startswith("-r "):
+                _, reqs_path = line.split(" ")
+                reqs.extend(_parse_requirements(reqs_path.strip()))
+            else:
+                reqs.append(line.strip())
+
+    return reqs
 
 
 setup(
@@ -14,6 +26,7 @@ setup(
     author="Kirill Brodt",
     install_requires=parse_requirements("./requirements/runtime.txt"),
     extras_require={
+        "all": parse_requirements("./requirements.txt"),
         "optional": parse_requirements("./requirements/optional.txt"),
         "interactive": parse_requirements("./requirements/interactive.txt"),
         "tests": parse_requirements("./requirements/tests.txt"),
