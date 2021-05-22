ee1aa1c09ced6758d401bd1ee716711222eb0be2
Kirill Brodt <cyrill.brodt@gmail.com>
2021-05-22 09:57:49 -0400

    Add sensors for train and predict dags

--- airflow_ml_dags/dags/predict.py
+++ airflow_ml_dags/dags/predict.py
@@ -1,8 +1,10 @@
 import datetime
+from pathlib import Path
 
 from airflow import DAG
 from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.sensors.python import PythonSensor
 from airflow.utils.dates import days_ago
 
 default_args = {
@@ -15,12 +17,31 @@ DATA_PATH = Variable.get("data_path")
 MODEL_PATH = Variable.get("model_path")
 
 
+def wait_fot_data_and_model(input_data_dir):
+    input_data_path = Path(input_data_dir)
+    model_path = Path(MODEL_PATH)
+
+    return (input_data_path / "data.csv").exists() and model_path.exists()
+
+
 with DAG(
     "predict",
     default_args=default_args,
     schedule_interval="@daily",
     start_date=days_ago(8),
 ) as dag:
+    wait_data_and_model = PythonSensor(
+        task_id="wait-for-data-and-model",
+        python_callable=wait_fot_data_and_model,
+        op_kwargs={
+            "input_data_dir": "/data/raw/{{ ds }}",
+        },
+        timeout=6000,
+        poke_interval=10,
+        retries=100,
+        mode="poke",
+    )
+
     cmd = (
         "--input-data /data/raw/{{ ds }}/data.csv"
         f" --model-path {MODEL_PATH}"
@@ -34,3 +55,5 @@ with DAG(
         do_xcom_push=False,
         volumes=[f"{DATA_PATH}:/data"],
     )
+
+    wait_data_and_model >> predict
--- airflow_ml_dags/dags/train_pipeline.py
+++ airflow_ml_dags/dags/train_pipeline.py
@@ -1,8 +1,10 @@
 import datetime
+from pathlib import Path
 
 from airflow import DAG
 from airflow.models import Variable
 from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.sensors.python import PythonSensor
 from airflow.utils.dates import days_ago
 
 default_args = {
@@ -12,6 +14,14 @@ default_args = {
     "retry_delay": datetime.timedelta(minutes=5),
 }
 DATA_PATH = Variable.get("data_path")
+
+
+def wait_fot_data_and_target(input_data_dir):
+    input_data_path = Path(input_data_dir)
+
+    return (input_data_path / "data.csv").exists() and (
+        input_data_path / "target.csv"
+    ).exists()
 
 
 def generate_operator(task_id, command):
@@ -28,11 +38,23 @@ def generate_operator(task_id, command):
 
 
 with DAG(
-    "train_pipeine",
+    "train_pipeline",
     default_args=default_args,
     schedule_interval="@weekly",
     start_date=days_ago(8),
 ) as dag:
+    wait_data_and_target = PythonSensor(
+        task_id="wait-for-data-and-target",
+        python_callable=wait_fot_data_and_target,
+        op_kwargs={
+            "input_data_dir": "/data/raw/{{ ds }}",
+        },
+        timeout=6000,
+        poke_interval=10,
+        retries=100,
+        mode="poke",
+    )
+
     cmd = (
         "process --input-data /data/raw/{{ ds }} --output-data /data/processed/{{ ds }}"
     )
@@ -47,4 +69,4 @@ with DAG(
     cmd = "validate --config /config.yml --input-data /data/processed/{{ ds }} --input-model /data/models/{{ ds }}"
     validate = generate_operator("validate", cmd)
 
-    preprocessing >> split >> train >> validate
+    wait_data_and_target >> preprocessing >> split >> train >> validate
